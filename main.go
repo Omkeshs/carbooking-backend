837187f8202e5decb716fa135864b7a7067d9142
omkesh1 <omkesh@scalent.io>
2020-09-10 00:45:07 +0530

    solved cors issue

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"std/omkesh/carBooking-Backend/database"
 	"std/omkesh/carBooking-Backend/endpoints"
+	"time"
 
 	handler "std/omkesh/carBooking-Backend/handlers"
 	repository "std/omkesh/carBooking-Backend/repositories"
 	service "std/omkesh/carBooking-Backend/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +21,19 @@ const (
 func main() {
 	fmt.Println("STEP 1 : FROM MAIN")
 	router := gin.Default()
+
+	//CORS ISSUE
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "GET", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:8080"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	conPool := database.NewMysqlConnection()
 	taskRepository := repository.NewRepositoryImpl(conPool)
